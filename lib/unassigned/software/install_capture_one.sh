c933678a97e78a870f26fe837521b14502374745
souari1974 <139868804+souari1974@users.noreply.github.com>
2026-04-27 19:43:00 +0200

    Update Capture One to v16.7.7.19

--- lib/unassigned/software/install_capture_one.sh
+++ lib/unassigned/software/install_capture_one.sh
@@ -2,5 +2,3 @@
 # Fleet passes the PKG path via $INSTALLER_PATH
 # This just runs the PKG, which contains the real install logic as postinstall script
 installer -pkg "$INSTALLER_PATH" -target /
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Capture One.app</string><key>_CFURLStringType</key><integer>0</integer></dict><key>file-label</key><string>Capture One</string></dict><key>tile-type</key><string>file-tile</string></dict>'
-killall Dock
